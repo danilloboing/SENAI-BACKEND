80d792c465f76793302cb382157b467a8146a067
DANILLO BOING DE SOUZA <danillo_souza@estudante.sesisenai.org.br>
2024-04-29 20:59:41 -0300

    OT12

---
+++ src/main/webapp/js/admin.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$("header").load("/projeto-trilha-web/pages/admin/components/header.html")
+	$("footer").load("/projeto-trilha-web/pages/admin/components/footer.html")
+})
